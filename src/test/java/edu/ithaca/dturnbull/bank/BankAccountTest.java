d5817f3c7eb3e53308995975f5812e0332d4a34f
gbrav0 <gbravo@ithaca.edu>
2025-01-28 10:54:49 -0500

    test

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -28,8 +28,16 @@ class BankAccountTest {
         assertFalse( BankAccount.isEmailValid(""));         // empty string
         assertFalse(BankAccount.isEmailValid("plainaddress")); // missing '@' and domain
         assertFalse(BankAccount.isEmailValid("user@.com")); // domain starts with dot
-
-        
+        assertFalse(BankAccount.isEmailValid("user@domain")); // missing domain
+        assertFalse(BankAccount.isEmailValid("a..b@gmail.com")); // consecutive dots in prefix
+        assertFalse(BankAccount.isEmailValid("gb@@mail.com")); // more than one '@'
+        assertFalse(BankAccount.isEmailValid(".hello@mail.com")); // starts with a special character
+        assertFalse(BankAccount.isEmailValid("user@domain..com")); // consecutive dots in domain
+        assertFalse(BankAccount.isEmailValid("user@domain,com")); // invalid character in domain
+        assertFalse(BankAccount.isEmailValid("user@domain.com ")); // ending space
+        assertFalse(BankAccount.isEmailValid(" user@domain.com")); // starting space 
+        assertFalse(BankAccount.isEmailValid("ab@mail.c"));
+        assertFalse(BankAccount.isEmailValid("ab@mail.c"));
     }
 
     @Test
