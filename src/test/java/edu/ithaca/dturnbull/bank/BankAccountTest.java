c27f6ee1ea22c240ae1a512ecbc10601f02c9ee3
1-Megan-1 <moconnor3@ithaca.edu>
2025-01-29 18:50:18 -0500

    Included comments on isEmailValid Function

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -25,20 +25,24 @@ class BankAccountTest {
     @Test
     void isEmailValidTest(){
         // hello hello test1 
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
-        assertFalse(BankAccount.isEmailValid("plainaddress")); // missing '@' and domain
-        assertFalse(BankAccount.isEmailValid("user@.com")); // domain starts with dot
-        assertFalse(BankAccount.isEmailValid("user@domain")); // missing domain
-        assertFalse(BankAccount.isEmailValid("a..b@gmail.com")); // consecutive dots in prefix
-        assertFalse(BankAccount.isEmailValid("gb@@mail.com")); // more than one '@'
-        assertFalse(BankAccount.isEmailValid(".hello@mail.com")); // starts with a special character
-        assertFalse(BankAccount.isEmailValid("user@domain..com")); // consecutive dots in domain
-        assertFalse(BankAccount.isEmailValid("user@domain,com")); // invalid chapracter in domain
-        assertFalse(BankAccount.isEmailValid("user@domain.com ")); // ending space
-        assertFalse(BankAccount.isEmailValid(" user@domain.com")); // starting space 
-        assertFalse(BankAccount.isEmailValid("ab@mail.c"));
-        assertFalse(BankAccount.isEmailValid("ab@mail.c"));
+        assertTrue(BankAccount.isEmailValid( "a@b.com"));   //Valid Partition: valid email address
+        assertFalse( BankAccount.isEmailValid(""));         //Invalid Border Case: empty string 
+        assertFalse(BankAccount.isEmailValid("plainaddress")); //Invalid Partition missing '@' and domain
+        assertFalse(BankAccount.isEmailValid("user@.com")); //Invalid Border Case: domain starts with dot
+        assertFalse(BankAccount.isEmailValid("user@domain")); //Invalid Partition:  missing domain
+        assertFalse(BankAccount.isEmailValid("a..b@gmail.com")); //Invalid Border Case: consecutive dots in prefix
+        assertFalse(BankAccount.isEmailValid("gb@@mail.com")); //Invalid Border Case: more than one '@'
+        assertFalse(BankAccount.isEmailValid(".hello@mail.com")); //Invalid Partition: starts with a special character
+        assertFalse(BankAccount.isEmailValid("user@domain..com")); //Invalid Border Case: consecutive dots in domain
+        assertFalse(BankAccount.isEmailValid("user@domain,com")); //Invalid Partition: invalid chapracter in domain
+        assertFalse(BankAccount.isEmailValid("user@domain.com ")); //Invalid Partition: ending space
+        assertFalse(BankAccount.isEmailValid(" user@domain.com")); //Invalid Partition: starting space 
+        assertFalse(BankAccount.isEmailValid("ab@mail.c")); //Invalid Boundary Case: domain suffix too short
+        assertFalse(BankAccount.isEmailValid("ab@mail.c")); //Invalid Boundary Case: domain suffix too short
+        //Potential Additional Tests: Check for the username ending in a '-'. Addresses are invalid if '-@' is present
+        //An address of above and at maximum length
+        //An address with too long of a domain suffix
+        //An address with a valid subdomain
     }
 
     @Test
